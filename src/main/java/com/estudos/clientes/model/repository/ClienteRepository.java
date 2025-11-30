1801e611f8e5a1f15b38d457b7fd3fc70613f9d6
Kay-Vogas <kay.vogas@gmail.com>
2025-11-30 17:54:41 -0300

    Modificação Repository

--- src/main/java/com/estudos/clientes/model/repository/ClienteRepository.java
+++ src/main/java/com/estudos/clientes/model/repository/ClienteRepository.java
@@ -2,6 +2,8 @@ package com.estudos.clientes.model.repository;
 
 import com.estudos.clientes.model.entities.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
 }
